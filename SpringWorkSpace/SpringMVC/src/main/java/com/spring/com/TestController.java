93f5d71631f513fd869da7ec7fc8f69c1b16c793
dksktjdrhks2 <52643441+dksktjdrhks2@users.noreply.github.com>
2020-02-03 15:06:07 +0900

    Add files via upload

--- SpringWorkSpace/SpringMVC/src/main/java/com/spring/com/TestController.java
+++ SpringWorkSpace/SpringMVC/src/main/java/com/spring/com/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 // Component
 // - Controller
@@ -38,9 +39,33 @@ public class TestController {
 		request.setAttribute("ip", ip);
 		//Model은 requset영역을 잠시 빌려서 바인딩 해놓은 구조이므로 온전한 request객체는 아니다...
 		//메소드의 매개변수로 request객체를 적어주면 자동으로 받아 온다....
-		
 		model.addAttribute("msg", msg);
 		
 		return VIEW_PATH + "test.jsp"; // /WEB-INF/views/test/test.jsp
+	}
+	
+	@RequestMapping("/test2.do")
+	public ModelAndView test2(HttpServletRequest request) {
+		
+		//ModelAndView
+		// - 데이터와 뷰 정보를 하나의 객체로 포장해서 전달할수 있는 객체,...
+		ModelAndView mv = new ModelAndView();
+		
+		String[] msg = new String[] {
+			"안녕",
+			"Hello",
+			"Sawubona",
+		};
+		
+		String ip = request.getRemoteAddr();
+		
+		mv.addObject("msg", msg);
+		mv.addObject("ip", ip);
+		
+		
+		//mv에 뷰 정보를 담는다...
+		mv.setViewName(VIEW_PATH + "test2.jsp");
+		
+		return mv;
 	}
 }
